338b66d7508ce9907527b9fff7c8e6e33a69d09b
Acce117 <ernestoalejandrocarraleroconde@gmail.com>
2023-02-10 12:38:14 -0800

    irrelevant changes

--- src/Playlist.java
+++ src/Playlist.java
@@ -8,20 +8,23 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Playlist {
     private ArrayList<Object> tracks;
     private int size;
     private String name;
+
     public Playlist(String name){
         this.name = name;
         this.tracks = new ArrayList<>();
         size = 0;
     }
+
+
     public Path getTrack(int index){
         return (Path)tracks.get(index);
     }
+
     public void addTrack(Path newTrack) throws IOException {
         if(Files.isDirectory(newTrack)){
             arrayProcess(newTrack);
@@ -31,30 +34,27 @@ public class Playlist {
             size++;
         }
     }
+
     public void removeTrack(int index){
         this.tracks.remove(index);
         size--;
     }
+
     public int getSize(){
         return size;
     }
 
     private void arrayProcess(Path newTrack) throws IOException {
-        Object[] newTracks = Arrays.stream(TreeModelCustomized.getInstance().getChildren(newTrack)).filter(path->{
-            boolean result = !Files.isDirectory((Path) path);
-            return result;
-        }).toArray();
+        Object[] aux =TreeModelCustomized.getInstance().getChildren(newTrack);
+        Object[] newTracks = Arrays.stream(aux).filter(path-> !Files.isDirectory((Path) path)).toArray();
 
         addTracks(newTracks);
     }
+
     public void addTracks(Object[] newTracks){
         this.tracks.addAll(List.of(newTracks));
         size = size + newTracks.length;
-        for(Object path: tracks){
-            System.out.println(((Path)path).toString());
-        }
     }
-
     public String getName() {
         return name;
     }
